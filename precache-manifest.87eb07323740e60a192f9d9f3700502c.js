e4d35d7de4ee5bc7e3e5b1c696602b1a7534c0f8
Cristian Ospina <crisospina523@gmail.com>
2019-05-11 23:40:27 -0500

    Updates

---
+++ precache-manifest.87eb07323740e60a192f9d9f3700502c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a962a0c0689031d6aced25e8b44026d",
+    "url": "/AppTareasReactJS/index.html"
+  },
+  {
+    "revision": "1d5a7e24af35410d88ba",
+    "url": "/AppTareasReactJS/static/css/main.eaab50b4.chunk.css"
+  },
+  {
+    "revision": "1dbc6b2763be16ba4094",
+    "url": "/AppTareasReactJS/static/js/2.e148b302.chunk.js"
+  },
+  {
+    "revision": "1d5a7e24af35410d88ba",
+    "url": "/AppTareasReactJS/static/js/main.12c79593.chunk.js"
+  },
+  {
+    "revision": "98fc04b9fd2dd3b312e8",
+    "url": "/AppTareasReactJS/static/js/runtime~main.321da283.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AppTareasReactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
